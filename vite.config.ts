4309865112e617422bf7adb6cca5d3122f9ca069
Yoshio Nishiyama <nishiyama@lvcap.jp>
2024-12-27 15:47:55 +0000

    :yen:viteの整備

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true, // 0.0.0.0 をバインド
+    port: 5173, // ポート番号を指定（デフォルトは 5173）
+    strictPort: true, // ポートが使用中ならエラーを出す
+    hmr: {               // ホットリロードの設定
+      protocol: 'ws',    // WebSocket プロトコルを指定
+      host: 'localhost', // 使用するホスト名
+    }
+  }
 })
